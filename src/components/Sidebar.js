025a3e14bd36a72dbc7f26ff36045106706c1fff
oopanpan <oopanpan@gmail.com>
2021-04-30 16:32:08 -0700

    sidebar boilderplate made

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Sidebar() {
+	return (
+		<div>
+			<ul>
+				<li>something with a link</li>
+				<li>Another Thing with a link</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Sidebar;
